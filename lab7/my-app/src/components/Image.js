f24c31aea46e8dfa439c2fb29352daf35ffa2e12
trush <trushchakolga@gmail.com>
2023-12-15 07:37:41 +0200

    lab7 task1

---
+++ lab7/my-app/src/components/Image.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Image() {
+    return (
+        <div id="imgContainer">
+            <img src="https://www.poland.travel/images/en-EN/cities/Gdansk/Gdanskkategoria.jpg" alt="Official" style={{width: '60%'}} />
+        </div>
+    );
+}
+
+export default Image;
